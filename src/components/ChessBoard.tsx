2d5ec50bc825f3a18487d62324293d0ff612880f
Nick Larew <nick.larew@10gen.com>
2020-03-12 16:47:24 -0400

    Organize code

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -19,12 +19,15 @@ export enum PlayerColor {
   Black = "black",
   White = "white"
 }
-interface Cell {
+export interface Cell {
   id: string;
   rank: RankID;
   file: FileID;
   color: PlayerColor;
 }
+
+const BOARD_HEIGHT = 800;
+const BOARD_WIDTH = 800;
 const CELLS: Array<Cell> = new Array(64).fill(0).map((_, index) => {
   const file = String.fromCharCode(65 + index / 8);
   const isOddRank = file.charCodeAt(0) % 2 === 1;
@@ -39,10 +42,7 @@ const CELLS: Array<Cell> = new Array(64).fill(0).map((_, index) => {
   };
 });
 
-const BOARD_HEIGHT = 800;
-const BOARD_WIDTH = 800;
-
-function useChessPieces(initialPieces: Array<ChessPieceConfiguration> = []) {
+function useChessPieces(initialPieces: Array<ChessPieceConfiguration>) {
   const [pieces, setPieces] = React.useState(initialPieces);
   return pieces;
 }
@@ -50,6 +50,7 @@ function useChessPieces(initialPieces: Array<ChessPieceConfiguration> = []) {
 type ChessBoardProps = {
   initialPieces: Array<ChessPieceConfiguration>;
 };
+
 export default function ChessBoard({ initialPieces = [] }: ChessBoardProps) {
   const pieces = useChessPieces(initialPieces);
   return (
@@ -69,14 +70,24 @@ export default function ChessBoard({ initialPieces = [] }: ChessBoardProps) {
     </ChessBoard.Layout>
   );
 }
+
 ChessBoard.Layout = styled.div`
   height: ${BOARD_HEIGHT}px;
   width: ${BOARD_WIDTH}px;
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-template-rows: repeat(8, 1fr);
-  grid-template-areas: ${boardGridAreas()};
+  grid-template-areas:
+    "A8 B8 C8 D8 E8 F8 G8 H8"
+    "A7 B7 C7 D7 E7 F7 G7 H7"
+    "A6 B6 C6 D6 E6 F6 G6 H6"
+    "A5 B5 C5 D5 E5 F5 G5 H5"
+    "A4 B4 C4 D4 E4 F4 G4 H4"
+    "A3 B3 C3 D3 E3 F3 G3 H3"
+    "A2 B2 C2 D2 E2 F2 G2 H2"
+    "A1 B1 C1 D1 E1 F1 G1 H1";
 `;
+
 ChessBoard.Cell = styled.div(
   (props: { cell: Cell }) => css`
     position: relative;
@@ -90,16 +101,8 @@ ChessBoard.Cell = styled.div(
     flex-direction: column;
   `,
 );
+
 ChessBoard.CellLabel = styled.span`
   position: absolute;
   padding: 4px;
 `;
-function boardGridAreas() {
-  const files = "ABCDEFGH".split("");
-  const ranks = "12345678".split("").reverse();
-  const gridAreas = [];
-  for (const rank of ranks) {
-    gridAreas.push(`"${files.map(f => f + rank).join(" ")}"`);
-  }
-  return gridAreas;
-}
